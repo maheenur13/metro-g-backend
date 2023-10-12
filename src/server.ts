4b915d3380a917fb514df51062c4ca57452a17b9
maheenur13 <differentmahee13@gmail.com>
2023-10-12 20:32:47 +0600

    refactored

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import { Server } from 'http';
 import app from './app';
-import subscribeToEvents from './app/events';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
-import { RedisClient } from './shared/redis';
 
 async function bootstrap() {
-  await RedisClient.connect().then(() => {
-    subscribeToEvents();
-  });
+  // await RedisClient.connect().then(() => {
+  //   subscribeToEvents();
+  // });
   const server: Server = app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`);
   });
