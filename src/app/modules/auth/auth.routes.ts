e81108b3a68d14931777be5d7ab9a2ff02e97bd2
maheenur13 <differentmahee13@gmail.com>
2023-10-14 05:15:26 +0600

    auth updated

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
@@ -18,10 +20,11 @@ router.post(
 router.post(
   '/change-password',
   validateRequest(AuthValidation.changePasswordZodSchema),
-  //   auth(
-  //     ENUM_USER_ROLE.SUPER_ADMIN,
-  //     ENUM_USER_ROLE.ADMIN,
-  //   ),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.CUSTOMER
+  ),
   AuthController.changePassword
 );
 
